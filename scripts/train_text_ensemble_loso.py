26e4c05b094bababe71f5f2168a98e22d8bc5b46
meatgoodbyeeee-wq <meat.good.byeeee@gmail.com>
2026-09-22 20:32:31 +0900

    Adopt FIN-blind structured tuning winner leaf8 max_features0.6

--- scripts/train_text_ensemble_loso.py
+++ scripts/train_text_ensemble_loso.py
@@ -22,7 +22,7 @@ def main():
  pred=np.zeros(len(d)); chosen={}
  for hold in sorted(sets):
   tr=d["set"]!=hold; te=~tr; y=d.loc[tr,"actual_gih"]
-  tree=make_pipeline(SimpleImputer(strategy="median"),ExtraTreesRegressor(n_estimators=600,min_samples_leaf=12,max_features=.8,n_jobs=-1,random_state=20260922))
+  tree=make_pipeline(SimpleImputer(strategy="median"),ExtraTreesRegressor(n_estimators=600,min_samples_leaf=8,max_features=.6,n_jobs=-1,random_state=20260922))
   tree.fit(d.loc[tr,nums],y); pt=tree.predict(d.loc[te,nums])
   txt=d["oracle_text"].fillna("")+" TYPE "+d["type_line"].fillna("")
   text=make_pipeline(TfidfVectorizer(ngram_range=(1,2),min_df=3,max_features=12000,sublinear_tf=True),Ridge(alpha=20))
